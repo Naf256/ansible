60e96711da494a574631a8208550c88bf573833f
Naf256 <69393971+Naf256@users.noreply.github.com>
2023-08-11 05:21:56 +0600

    added sudo to ansible playbook for apt permission

--- stowing.sh
+++ stowing.sh
@@ -29,5 +29,5 @@ sudo apt-add-repository -y ppa:ansible/ansible
 sudo apt-get update -y
 sudo apt-get install -y curl git software-properties-common ansible
 
-ansible-playbook --ask-vault-pass local.yml
+sudo ansible-playbook --ask-vault-pass local.yml
 
