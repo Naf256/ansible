d7c703a805286ca1807256eff8386ad9b4b9fcea
tokyaramen <nafis_256@yahoo.com>
2025-02-23 02:52:21 +0600

    never use this plugins again
    causing error detecting the buffer
    like not recognizing .c files

--- nvim/.config/nvim/lua/pluginlist.lua
+++ nvim/.config/nvim/lua/pluginlist.lua
@@ -1,9 +1,9 @@
 return {
 
 	-- starup time optimise
-	"dstein64/vim-startuptime",
-	"lewis6991/impatient.nvim",
-	"nathom/filetype.nvim",
+	-- "dstein64/vim-startuptime",
+	-- "lewis6991/impatient.nvim",
+	-- "nathom/filetype.nvim",
 
 	-- {
 	-- 	'akinsho/bufferline.nvim',
@@ -212,19 +212,19 @@ return {
 	--    { 'nvim-telescope/telescope-dap.nvim', opts = {} }
 
 	-- I am to ready yet for flashing
-	{
-		"folke/flash.nvim",
-		event = "VeryLazy",
-		---@type Flash.Config
-		opts = {},
-		-- stylua: ignore
-		keys = {
-			{ "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
-			{ ";S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
-			{ "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
-			{ "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
-			{ "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
-		},
-	}
+	-- {
+	-- 	"folke/flash.nvim",
+	-- 	event = "VeryLazy",
+	-- 	---@type Flash.Config
+	-- 	opts = {},
+	-- 	-- stylua: ignore
+	-- 	keys = {
+	-- 		{ "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+	-- 		{ ";S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+	-- 		{ "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+	-- 		{ "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+	-- 		{ "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+	-- 	},
+	-- }
 
 	}
